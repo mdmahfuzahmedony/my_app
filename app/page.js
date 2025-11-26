6b309122da6467cfd653dfd7f91e4f84ec5a763a
mdmahfuzahmedony <Mdmahfuzahmedony@gmail.com>
2025-11-26 21:41:28 +0600

    why_chooseus

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 
-import divider from "daisyui/components/divider";
 import FeatureProduct from "./Component/FeatureProduct";
 import HeroSection from "./Component/Herosection";
+import WhyChoose from "./Component/WhyChoose";
 
 export default function Home() {
   return (
     <div>
   <HeroSection></HeroSection>
    <FeatureProduct/>
+   <WhyChoose></WhyChoose>
     </div>
  
    
